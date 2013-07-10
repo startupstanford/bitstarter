5b8f68958d2f93bb3e5968a5bb61bdf6e501d851
OpenShift guest <startup.stanford@akakikou.com>
2013-07-10 17:28:07 -0400

    mpe

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var msg = fs.readFile("index.html", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(msg);
+  response.send(msg.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
