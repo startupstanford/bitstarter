d64bb8942e4c31c30dca12ff4c8f05b07484b8b3
OpenShift guest <startup.stanford@akakikou.com>
2013-07-10 17:25:46 -0400

    mpe

--- web.js
+++ web.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var fs = require('fs');
+var msg = fs.readFile("index.html", "utf-8");
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
